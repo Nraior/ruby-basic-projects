490ddeab2066ef9fcf389ba6d1620344a202b883
DN <dnowakowski200@gmail.com>
2025-04-23 00:10:35 +0200

    moved caesar cipher calling methods to its own file & created basic test for rspec

---
+++ caesar_cipher_main.rb
@@ -0,0 +1,8 @@
+require_relative 'caesar_cipher'
+puts caesar_cipher("xyz!", 3)
+puts caesar_cipher("abc!", -3)
+puts caesar_cipher("XYZ!", 3)
+puts caesar_cipher("ABC!", -3)
+puts caesar_cipher("What a string!", 5)
+puts caesar_cipher("What a string!", 31)
+#Bmfy f xywnsl!
---
+++ spec/caesar_cipher_spec.rb
@@ -0,0 +1,7 @@
+require './caesar_cipher.rb'
+
+describe '#caesar_cipher' do
+  it "Is ok" do
+    expect(true).to eq (true)
+  end
+end
